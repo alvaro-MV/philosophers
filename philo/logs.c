2738b16c7533a2eafb5548ea303ff4dc148a12fd
Alvaro Morales De Vera <alvmoral@c2r16s2.42madrid.com>
2025-03-19 17:28:23 +0100

    Revert "Now the logs print 0 at beginning."
    This reverts commit 9623454213ae29dee3a5886395d22784c8a1c524.

--- philo/logs.c
+++ philo/logs.c
@@ -6,7 +6,7 @@
 /*   By: alvmoral <alvmoral@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 13:39:52 by alvmoral          #+#    #+#             */
-/*   Updated: 2025/03/19 17:21:05 by alvmoral         ###   ########.fr       */
+/*   Updated: 2025/03/19 17:27:32 by alvmoral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void	fork_log(t_philo *dinner, size_t action)
 	uint64_t	time;
 
 	time = get_actual_time();
-	time = time - dinner->time_last_meal;
+	time = time - dinner->born_time;
 	if (action)
-		printf("\033[3;33m%lu %d has taken a fork\n", time, dinner->tid);
+		ft_printf("\033[3;33m%l %d has taken a fork\n", time, dinner->tid);
 	else
-		printf("\033[3;36m%lu %d has release a forks\n", time, dinner->tid);
+		ft_printf("\033[3;36m%l %d has release a forks\n", time, dinner->tid);
 }
 
 void	eating_log(t_philo *dinner)
@@ -38,7 +38,7 @@ void	sleeping_log(t_philo *dinner)
 	uint64_t	time;
 
 	time = get_actual_time();
-	time = time - dinner->time_last_meal;
+	time = time - dinner->born_time;
 	ft_printf("\033[3;35m%l %d is sleeping\n", time, dinner->tid);
 }
 
@@ -47,7 +47,7 @@ void	thinking_log(t_philo *dinner)
 	uint64_t	time;
 
 	time = get_actual_time();
-	time = time - dinner->time_last_meal;
+	time = time - dinner->born_time;
 	ft_printf("\033[3;34m%l %d is thinking\n", time, dinner->tid);
 }
 
@@ -56,6 +56,6 @@ void	died_log(t_philo *dinner)
 	uint64_t	time;
 
 	time = get_actual_time();
-	time = time - dinner->time_last_meal;
+	time = time - dinner->born_time;
 	ft_printf("\033[3;31m%l %d died\n\033[0m", time, dinner->tid);
 }
