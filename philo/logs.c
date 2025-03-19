7c2edcbc41fc1f0bb337428411b8e0e070dae278
Alvaro Morales De Vera <alvmoral@c2r16s2.42madrid.com>
2025-03-19 17:05:27 +0100

    Parece que en el cluster, tras pequeñas modificaciones en la función manage_usleep el comportamiento es correcto.

--- philo/logs.c
+++ philo/logs.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   logs.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alvaro <alvaro@student.42.fr>              +#+  +:+       +#+        */
+/*   By: alvmoral <alvmoral@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 13:39:52 by alvmoral          #+#    #+#             */
-/*   Updated: 2025/03/19 16:17:59 by alvaro           ###   ########.fr       */
+/*   Updated: 2025/03/19 16:33:27 by alvmoral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	fork_log(t_philo *dinner, size_t action)
 	uint64_t	time;
 
 	time = get_actual_time();
-	time = time - dinner->time_last_meal;
+	time = time - dinner->born_time;
 	if (action)
 		printf("\033[3;33m%lu %d has taken a fork\n", time, dinner->tid);
 	else
@@ -29,7 +29,7 @@ void	eating_log(t_philo *dinner)
 	uint64_t	time;
 
 	time = get_actual_time();
-	time = time - dinner->time_last_meal;
+	time = time - dinner->born_time;
 	printf("\033[3;32m%lu %d is eating\n", time, dinner->tid);
 }
 
@@ -38,7 +38,7 @@ void	sleeping_log(t_philo *dinner)
 	uint64_t	time;
 
 	time = get_actual_time();
-	time = time - dinner->time_last_meal;
+	time = time - dinner->born_time;
 	ft_printf("\033[3;35m%l %d is sleeping\n", time, dinner->tid);
 }
 
@@ -47,7 +47,7 @@ void	thinking_log(t_philo *dinner)
 	uint64_t	time;
 
 	time = get_actual_time();
-	time = time - dinner->time_last_meal;
+	time = time - dinner->born_time;
 	ft_printf("\033[3;34m%l %d is thinking\n", time, dinner->tid);
 }
 
@@ -56,6 +56,6 @@ void	died_log(t_philo *dinner)
 	uint64_t	time;
 
 	time = get_actual_time();
-	time = time - dinner->time_last_meal;
+	time = time - dinner->born_time;
 	ft_printf("\033[3;31m%l %d died\n\033[0m", time, dinner->tid);
 }
