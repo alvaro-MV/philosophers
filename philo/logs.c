69c9b5e77557d18af22af6a1ff5705240c69768b
Alvaro Morales De Vera <alvmoral@c2r16s2.42madrid.com>
2025-03-19 17:15:44 +0100

    commit for reverting to 9623454213ae29dee3a5886395d22784c8a1c524

--- philo/logs.c
+++ philo/logs.c
@@ -6,7 +6,7 @@
 /*   By: alvmoral <alvmoral@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 13:39:52 by alvmoral          #+#    #+#             */
-/*   Updated: 2025/03/19 16:33:27 by alvmoral         ###   ########.fr       */
+/*   Updated: 2025/03/19 17:14:58 by alvmoral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ void	fork_log(t_philo *dinner, size_t action)
 	time = get_actual_time();
 	time = time - dinner->born_time;
 	if (action)
-		printf("\033[3;33m%lu %d has taken a fork\n", time, dinner->tid);
+		ft_printf("\033[3;33m%l %d has taken a fork\n", time, dinner->tid);
 	else
-		printf("\033[3;36m%lu %d has release a forks\n", time, dinner->tid);
+		ft_printf("\033[3;36m%l %d has release a forks\n", time, dinner->tid);
 }
 
 void	eating_log(t_philo *dinner)
@@ -29,7 +29,7 @@ void	eating_log(t_philo *dinner)
 	uint64_t	time;
 
 	time = get_actual_time();
-	time = time - dinner->born_time;
+	time = time - dinner->time_last_meal;
 	printf("\033[3;32m%lu %d is eating\n", time, dinner->tid);
 }
 
